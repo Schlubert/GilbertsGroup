e1dc752d63a3717522bfdbc70b7190f56c7df6b8
Kevin <schlubert@gmail.com>
2025-11-28 16:42:57 +1300

    whatever

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -126,7 +126,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                   The
                 </div>
                 <img
-                  src={"${BASE_PATH}images/gilberts-group-logo-white.svg"}
+                  src="images/GilbertsGroupLogo.svg"
                   alt="Gilbert's Group Logo"
                   className="h-10 w-auto mb-1"
                 />
